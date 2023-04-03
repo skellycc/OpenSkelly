1e17127c84ddb3d5854a09a4f45124971e09de11
shabman <avia.shabbyman@gmail.com>
2023-04-03 12:52:57 +0100

    first push

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * The OpenSkelly Project
+ *
+ * Developed by The Skelly Compiler Collection software foundation team.
+ *
+ * Copyright (C) 2023 SCC. See the LICENSE for more information.
+ *
+ * Date Created: 03/04/2023
+ * Authors:
+ * 		@shabman - Mustafa Malik
+ *
+ * This file is part of the SCC-VM project (Skelly Compiler Collection Virtual Machines)
+ *
+ * Developed for the SCMLVM system. (Skelly Compiler Middle Level Virtual Machine)
+ */
+
+#include <iostream>
+
+int main(void) {
+	std::cout << "test!" << std::endl;
+}
