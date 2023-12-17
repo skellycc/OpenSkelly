59c797e8fe64fb84f1f6305e8f910e7a798e4d9b
shabman <avia.shabbyman@gmail.com>
2023-12-17 23:16:24 +0000

    Initial Commit

---
+++ OpenSkelly/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  OpenSkelly
+//
+//  Created by Mustafa Malik on 17/12/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
