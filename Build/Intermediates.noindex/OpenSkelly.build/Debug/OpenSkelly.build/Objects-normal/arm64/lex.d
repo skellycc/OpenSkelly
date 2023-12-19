28f5edad29a3909caec5eaf0656d7d1ac8dfa33b
shabman <avia.shabbyman@gmail.com>
2023-12-19 00:47:58 +0000

    first push

--- Build/Intermediates.noindex/OpenSkelly.build/Debug/OpenSkelly.build/Objects-normal/arm64/lex.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/mustafamalik/Documents/Skelly/OpenSkelly/OpenSkelly/lex.c \
-  /Users/mustafamalik/Documents/Skelly/OpenSkelly/OpenSkelly/include/lex.h \
-  /Users/mustafamalik/Documents/Skelly/OpenSkelly/OpenSkelly/include/decls.h \
-  /Users/mustafamalik/Documents/Skelly/OpenSkelly/OpenSkelly/include/token.h \
-  /Users/mustafamalik/Documents/Skelly/OpenSkelly/OpenSkelly/include/keywords.h
--- Build/Intermediates.noindex/OpenSkelly.build/Debug/OpenSkelly.build/Objects-normal/arm64/main.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/mustafamalik/Documents/Skelly/OpenSkelly/OpenSkelly/main.c \
-  /Users/mustafamalik/Documents/Skelly/OpenSkelly/OpenSkelly/include/lex.h \
-  /Users/mustafamalik/Documents/Skelly/OpenSkelly/OpenSkelly/include/decls.h \
-  /Users/mustafamalik/Documents/Skelly/OpenSkelly/OpenSkelly/include/token.h
